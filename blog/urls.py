013c835c3d2c442911a99b43cb17d4aa4644c195
GrVladimir <Gt_vova@mail.ru>
2020-08-24 13:54:12 +0300

    final1

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
- 
+from . import views
 from .views import BlogListView, BlogDetailView 
  
 urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), # новое изменение
     path('', BlogListView.as_view(), name='home'),
+    path('post/new/', views.post_new, name='post_new')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import ListView, DetailView
- 
+from .forms import PostForm
 from .models import Post
+from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import redirect
  
  
 class BlogListView(ListView):
@@ -10,3 +13,18 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView): 
     model = Post
     template_name = 'post_detail.html'
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post_edit.html', {'form': form})
+
+  
